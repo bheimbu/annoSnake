862c4c06e8ed089ae2bcdc66319ccf0c7599a4c8
bheimbu <bheimbu@gwdg.de>
2024-03-06 13:13:40 +0100

    Create cazymes_visualize.R

---
+++ workflow/rules/scripts/cazymes_visualize.R
@@ -0,0 +1,109 @@
+# Load libraries
+library(dplyr)
+library(propr)
+library(ggplot2)
+library(tidyr)
+library(compositions)
+library(ape)
+library(vegan)
+
+#data wrangling####
+data <- read.table(snakemake@input[['hits']], header = FALSE, sep = "\t")
+result <- data[, c(2, 6)]
+as_tibble(result)
+
+gtf <- read.table(snakemake@input[['gtf']], sep = "\t", header = FALSE, stringsAsFactors = FALSE)
+
+result2 <- data.frame(gtf[, 1], gsub(".*gene_id\\s+", "", gtf[, 9]))
+colnames(result2) <- c("V1", "V2")
+
+merged_result <- merge(result, result2, by.x = "V2", by.y = "V2", all.x = TRUE, all.y = FALSE)
+merged_result <- merged_result[, c("V1", "V2", "V6")]
+
+table_data <- read.table(snakemake@input[['sf']], sep = "\t", header = TRUE)
+table_data$Name <- gsub(":.*$", "", table_data$Name)
+
+merged_result2 <- merge(merged_result, table_data, by.x = "V1", by.y = "Name", all.x = TRUE, all.y = FALSE)
+colnames(merged_result2) <- c("contig_names", "protein_names", "cazyme", "length", "effective_length", "tpm", "num_reads")
+
+merged_result_filtered <- merged_result2 %>%
+  filter(num_reads >= 10 & tpm >= 1)
+
+merged_result_filtered <- as_tibble(merged_result_filtered)
+
+merged_result_filtered  <- merged_result_filtered  %>%
+  mutate(tpm = as.numeric(tpm))
+
+merged_result_filtered$log_tpm <- log10(merged_result_filtered$tpm + 1)
+
+merged_result_filtered <- merged_result_filtered %>%
+  distinct(contig_names, cazyme, log_tpm)
+
+spread <- spread(merged_result_filtered, cazyme, log_tpm)
+
+aggregated_data <- spread %>%
+  group_by(contig_names) %>%
+  summarise_all(~ if (is.numeric(.)) sum(., na.rm = TRUE) else first(.))
+
+contig <- spread %>%
+  mutate(contig_names = sub("_.*", "", contig_names))
+
+aggregated_data <- contig %>%
+  group_by(contig_names) %>%
+  summarise(across(everything(), sum, na.rm = TRUE))
+
+aggregated_data <- aggregated_data %>%
+  rename(sample = contig_names)
+
+clr_data_subset <- aggregated_data[-which(names(aggregated_data) == "sample")]
+
+clr <- decostand(clr_data_subset, method = "clr", pseudocount = 1)
+
+clr_result <- cbind(sample = aggregated_data$sample, clr)
+clr_result <- as_tibble(clr_result)
+
+transposed_clr_result <- clr_result %>% t() %>% as.data.frame()
+colnames(transposed_clr_result) <- transposed_clr_result[1, ]
+transposed_clr_result[, -1] <- lapply(transposed_clr_result[, -1], as.numeric)
+transposed_clr_result <- transposed_clr_result[-1, ]
+transposed_clr_result <- transposed_clr_result %>% tibble::rownames_to_column(var = "Cazyme")
+transposed_clr_result <- transposed_clr_result %>%  mutate(across(2:ncol(transposed_clr_result), as.numeric))
+as_tibble(transposed_clr_result)
+transposed_clr_result$TotalAbundance <- rowSums(transposed_clr_result[, -1])  # Calculate the row-wise abundances
+
+top_50_cazymes <- transposed_clr_result[order(transposed_clr_result$TotalAbundance, decreasing = TRUE), ][1:50, ]
+top_50_cazymes <- top_50_cazymes[, -ncol(top_50_cazymes)]
+top_50_cazymes <- t(top_50_cazymes)
+colnames(top_50_cazymes) <- top_50_cazymes[1, ]
+top_50_cazymes <- top_50_cazymes[-1, ]
+top_50_cazymes <- as.data.frame(top_50_cazymes)
+top_50_cazymes$sample <- rownames(top_50_cazymes)
+rownames(top_50_cazymes) <- NULL
+
+# Reshape data to long format
+clr_result_long <- gather(as.data.frame(top_50_cazymes), cazyme, clr_value, -sample)
+clr_result_long$clr_value <- as.numeric(clr_result_long$clr_value)
+clr_result_long$cazyme <- as.factor(clr_result_long$cazyme)
+clr_result_long$cazyme <- sub("\\.hmm", "", clr_result_long$cazyme)
+
+#save as pdf#### 
+heatmap <- ggplot(data = clr_result_long, aes(x = sample, y = cazyme, fill = clr_value, text = sample, label = clr_value, label2 = cazyme)) +
+  geom_tile() +
+  geom_tile(fill = NA, color = "black", size = 0.1) +
+  scale_fill_viridis_c(option="viridis", direction = 1, name = "log(TPM+1)") +
+  theme_minimal() +
+  theme(axis.title.y = element_blank(),
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(angle = 90, hjust = .75, vjust = .25, face = "bold"),
+        axis.text.y = element_text(face = "bold"),
+        legend.text = element_text(face = "bold"),
+        legend.title = element_text(face = "bold"))
+
+pdf(NULL)
+pdf(snakemake@output[['pdf']], paper = "a4r", width = 30, height = 15)
+heatmap
+dev.off()
+
+#save as html####
+p <- ggplotly(heatmap, tooltip = c("text","label","label2"))
+htmlwidgets::saveWidget(p, snakemake@output[['html']])
