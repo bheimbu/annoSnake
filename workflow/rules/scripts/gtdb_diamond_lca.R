e43f521b95e7b171af1577dbb9a7b23279a60624
bheimbu <bheimbu@gwdg.de>
2024-03-21 14:08:00 +0100

    Update gtdb_diamond_lca.R

--- workflow/rules/scripts/gtdb_diamond_lca.R
+++ workflow/rules/scripts/gtdb_diamond_lca.R
@@ -1,12 +1,12 @@
 #!/usr/bin/env Rscript
 
 library(dplyr)
+library(tidyverse)
 
-taxa<-read.table(file.path(snakemake@params[["lca"]],"gtdb_vers202_lca.csv"),header=TRUE, sep=",")
+taxa<-read.csv(file.path(snakemake@params[["lca"]],"gtdb_vers202_lca.csv"),header=TRUE, sep=",")
 taxa$X<-NULL
-
-blast<-read.csv(snakemake@input[["input"]],header=FALSE,sep="\t")
-blast2<-blast%>%filter(V3<=snakemake@params[["evalue"]])
-
+blast<-read.delim(snakemake@input[["input"]], header = F, sep = "\t", colClasses = "character")
+options(scipen = 1)
+blast2 <- subset(blast, V3 <= snakemake@params[["evalue"]])
 taxa_blast<-merge(blast2,taxa,by.x="V2",by.y="taxID")
 write.csv(taxa_blast,snakemake@output[["output"]])
